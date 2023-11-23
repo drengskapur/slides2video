d3c8addc596a7c97909e7742f7a1db44d500aa85
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2023-11-23 07:24:00 -0500

    Update files.js

--- src/files.js
+++ src/files.js
@@ -3,152 +3,152 @@
 /**
  * @fileoverview Helpers for google.colab Python module.
  */
-(function(scope) {
-function span(text, styleAttributes = {}) {
-  const element = document.createElement('span');
-  element.textContent = text;
-  for (const key of Object.keys(styleAttributes)) {
-    element.style[key] = styleAttributes[key];
-  }
-  return element;
-}
-
-// Max number of bytes which will be uploaded at a time.
-const MAX_PAYLOAD_SIZE = 100 * 1024;
-
-function _uploadFiles(inputId, outputId) {
-  const steps = uploadFilesStep(inputId, outputId);
-  const outputElement = document.getElementById(outputId);
-  // Cache steps on the outputElement to make it available for the next call
-  // to uploadFilesContinue from Python.
-  outputElement.steps = steps;
-
-  return _uploadFilesContinue(outputId);
-}
-
-// This is roughly an async generator (not supported in the browser yet),
-// where there are multiple asynchronous steps and the Python side is going
-// to poll for completion of each step.
-// This uses a Promise to block the python side on completion of each step,
-// then passes the result of the previous step as the input to the next step.
-function _uploadFilesContinue(outputId) {
-  const outputElement = document.getElementById(outputId);
-  const steps = outputElement.steps;
-
-  const next = steps.next(outputElement.lastPromiseValue);
-  return Promise.resolve(next.value.promise).then((value) => {
-    // Cache the last promise value to make it available to the next
-    // step of the generator.
-    outputElement.lastPromiseValue = value;
-    return next.value.response;
-  });
-}
-
-/**
- * Generator function which is called between each async step of the upload
- * process.
- * @param {string} inputId Element ID of the input file picker element.
- * @param {string} outputId Element ID of the output display.
- * @return {!Iterable<!Object>} Iterable of next steps.
- */
-function* uploadFilesStep(inputId, outputId) {
-  const inputElement = document.getElementById(inputId);
-  inputElement.disabled = false;
-
-  const outputElement = document.getElementById(outputId);
-  outputElement.innerHTML = '';
-
-  const pickedPromise = new Promise((resolve) => {
-    inputElement.addEventListener('change', (e) => {
-      resolve(e.target.files);
-    });
-  });
-
-  // Wait for the user to pick the files.
-  const files = yield {
-    promise: pickedPromise,
-    response: {
-      action: 'starting',
+(function (scope) {
+  function span(text, styleAttributes = {}) {
+    const element = document.createElement('span');
+    element.textContent = text;
+    for (const key of Object.keys(styleAttributes)) {
+      element.style[key] = styleAttributes[key];
     }
-  };
+    return element;
+  }
 
-  inputElement.disabled = true;
+  // Max number of bytes which will be uploaded at a time.
+  const MAX_PAYLOAD_SIZE = 100 * 1024;
 
-  if (!files) {
-    return {
+  function _uploadFiles(inputId, outputId) {
+    const steps = uploadFilesStep(inputId, outputId);
+    const outputElement = document.getElementById(outputId);
+    // Cache steps on the outputElement to make it available for the next call
+    // to uploadFilesContinue from Python.
+    outputElement.steps = steps;
+
+    return _uploadFilesContinue(outputId);
+  }
+
+  // This is roughly an async generator (not supported in the browser yet),
+  // where there are multiple asynchronous steps and the Python side is going
+  // to poll for completion of each step.
+  // This uses a Promise to block the python side on completion of each step,
+  // then passes the result of the previous step as the input to the next step.
+  function _uploadFilesContinue(outputId) {
+    const outputElement = document.getElementById(outputId);
+    const steps = outputElement.steps;
+
+    const next = steps.next(outputElement.lastPromiseValue);
+    return Promise.resolve(next.value.promise).then((value) => {
+      // Cache the last promise value to make it available to the next
+      // step of the generator.
+      outputElement.lastPromiseValue = value;
+      return next.value.response;
+    });
+  }
+
+  /**
+   * Generator function which is called between each async step of the upload
+   * process.
+   * @param {string} inputId Element ID of the input file picker element.
+   * @param {string} outputId Element ID of the output display.
+   * @return {!Iterable<!Object>} Iterable of next steps.
+   */
+  function* uploadFilesStep(inputId, outputId) {
+    const inputElement = document.getElementById(inputId);
+    inputElement.disabled = false;
+
+    const outputElement = document.getElementById(outputId);
+    outputElement.innerHTML = '';
+
+    const pickedPromise = new Promise((resolve) => {
+      inputElement.addEventListener('change', (e) => {
+        resolve(e.target.files);
+      });
+    });
+
+    // Wait for the user to pick the files.
+    const files = yield {
+      promise: pickedPromise,
+      response: {
+        action: 'starting',
+      }
+    };
+
+    inputElement.disabled = true;
+
+    if (!files) {
+      return {
+        response: {
+          action: 'complete',
+        }
+      };
+    }
+
+    for (const file of files) {
+      // Create and append the progress bar
+      const progressBar = document.createElement('progress');
+      progressBar.max = file.size;
+      progressBar.value = 0;
+      outputElement.appendChild(progressBar);
+
+      // Create and append the percentage span
+      const percent = span(' 0% ');
+      outputElement.appendChild(percent);
+
+      // Append the file name
+      outputElement.appendChild(span(file.name, { fontWeight: 'bold' }));
+      outputElement.appendChild(span(` (${file.size} bytes) `));
+
+      const fileDataPromise = new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          resolve(e.target.result);
+        };
+        reader.readAsArrayBuffer(file);
+      });
+
+      let fileData = yield {
+        promise: fileDataPromise,
+        response: {
+          action: 'continue',
+        }
+      };
+
+      let position = 0;
+      do {
+        const length = Math.min(fileData.byteLength - position, MAX_PAYLOAD_SIZE);
+        const chunk = new Uint8Array(fileData, position, length);
+        position += length;
+
+        const base64 = btoa(String.fromCharCode.apply(null, chunk));
+        yield {
+          response: {
+            action: 'append',
+            file: file.name,
+            data: base64,
+          },
+        };
+
+        let percentDone = fileData.byteLength === 0 ?
+          100 :
+          Math.round((position / fileData.byteLength) * 100);
+        percent.textContent = ` ${percentDone}% `;
+        progressBar.value = position;
+
+      } while (position < fileData.byteLength);
+    }
+
+    // All done.
+    yield {
       response: {
         action: 'complete',
       }
     };
   }
 
-  for (const file of files) {
-    // Create and append the progress bar
-    const progressBar = document.createElement('progress');
-    progressBar.max = file.size;
-    progressBar.value = 0;
-    outputElement.appendChild(progressBar);
 
-    // Create and append the percentage span
-    const percent = span(' 0% ');
-    outputElement.appendChild(percent);
-
-    // Append the file name
-    outputElement.appendChild(span(file.name, {fontWeight: 'bold'}));
-    outputElement.appendChild(span(` (${file.size} bytes) `));
-
-    const fileDataPromise = new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        resolve(e.target.result);
-      };
-      reader.readAsArrayBuffer(file);
-    });
-
-    let fileData = yield {
-      promise: fileDataPromise,
-      response: {
-        action: 'continue',
-      }
-    };
-
-    let position = 0;
-    do {
-      const length = Math.min(fileData.byteLength - position, MAX_PAYLOAD_SIZE);
-      const chunk = new Uint8Array(fileData, position, length);
-      position += length;
-
-      const base64 = btoa(String.fromCharCode.apply(null, chunk));
-      yield {
-        response: {
-          action: 'append',
-          file: file.name,
-          data: base64,
-        },
-      };
-
-      let percentDone = fileData.byteLength === 0 ?
-          100 :
-          Math.round((position / fileData.byteLength) * 100);
-      percent.textContent = ` ${percentDone}% `;
-      progressBar.value = position;
-
-    } while (position < fileData.byteLength);
-  }
-
-  // All done.
-  yield {
-    response: {
-      action: 'complete',
-    }
+  scope.google = scope.google || {};
+  scope.google.colab = scope.google.colab || {};
+  scope.google.colab._files = {
+    _uploadFiles,
+    _uploadFilesContinue,
   };
-}
-
-
-scope.google = scope.google || {};
-scope.google.colab = scope.google.colab || {};
-scope.google.colab._files = {
-  _uploadFiles,
-  _uploadFilesContinue,
-};
 })(self);
