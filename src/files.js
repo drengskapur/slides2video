11a796403447d06a95bcffe36bdd13cbef643c6d
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2023-11-24 11:14:16 -0500

    Update files.js

--- src/files.js
+++ src/files.js
@@ -1,121 +1,95 @@
 // ADAPTED FROM: https://github.com/googlecolab/colabtools/blob/main/google/colab/resources/files.js
 (function (scope) {
-function span(text, styleAttributes = {}) {
-    const element = document.createElement('span');
-    element.textContent = text;
-    for (const key of Object.keys(styleAttributes)) {
-        element.style[key] = styleAttributes[key];
+    const MAX_PAYLOAD_SIZE = 100 * 1024;
+    const fileReader = new FileReader();
+
+    function span(text, styleAttributes = {}) {
+        const element = document.createElement('span');
+        element.textContent = text;
+        Object.assign(element.style, styleAttributes);
+        return element;
     }
-    return element;
-}
 
-const MAX_PAYLOAD_SIZE = 100 * 1024;
-
-function _uploadFiles(inputId, outputId) {
-    const steps = uploadFilesStep(inputId, outputId);
-    const outputElement = document.getElementById(outputId);
-    outputElement.steps = steps;
-    return _uploadFilesContinue(outputId);
-}
-
-function _uploadFilesContinue(outputId) {
-    const outputElement = document.getElementById(outputId);
-    const steps = outputElement.steps;
-    const next = steps.next(outputElement.lastPromiseValue);
-    return Promise.resolve(next.value.promise).then((value) => {
-        outputElement.lastPromiseValue = value;
-        return next.value.response;
-    });
-}
-
-function* uploadFilesStep(inputId, outputId) {
-    const inputElement = document.getElementById(inputId);
-    inputElement.disabled = false;
-    const outputElement = document.getElementById(outputId);
-    outputElement.innerHTML = '';
-    const pickedPromise = new Promise((resolve) => {
-        inputElement.addEventListener('change', (e) => {
-            resolve(e.target.files);
-        });
-    });
-
-    const files = yield {
-        promise: pickedPromise,
-        response: {
-            action: 'starting',
+    function updateProgressBar(percentDone) {
+        const pythonProgressBar = document.querySelector('div.progress > div.progress-bar');
+        if (pythonProgressBar) {
+            pythonProgressBar.style.width = `${percentDone}%`;
         }
-    };
-
-    inputElement.disabled = true;
-
-    if (!files) {
-        return {
-            response: {
-                action: 'complete',
-            }
-        };
     }
 
-    for (const file of files) {
+    async function processFile(file) {
+        return new Promise((resolve, reject) => {
+            fileReader.onload = (e) => resolve(e.target.result);
+            fileReader.onerror = (e) => reject(e);
+            fileReader.readAsArrayBuffer(file);
+        });
+    }
 
-        const fileDataPromise = new Promise((resolve) => {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                resolve(e.target.result);
-            };
-            reader.readAsArrayBuffer(file);
+    async function* uploadFilesStep(inputId) {
+        const inputElement = document.getElementById(inputId);
+        inputElement.disabled = false;
+        inputElement.accept = '.pptx, application/vnd.openxmlformats-officedocument.presentationml.presentation';
+
+        const files = await new Promise((resolve) => {
+            inputElement.addEventListener('change', (e) => resolve(e.target.files));
         });
 
-        let fileData = yield {
-            promise: fileDataPromise,
-            response: {
-                action: 'continue',
-            }
-        };
+        inputElement.disabled = true;
+        if (!files) return;
 
-        let position = 0;
+        for (const file of files) {
+            let fileData = await processFile(file);
+            let position = 0;
 
-        do {
-            const length = Math.min(fileData.byteLength - position, MAX_PAYLOAD_SIZE);
-            const chunk = new Uint8Array(fileData, position, length);
-            position += length;
-            const base64 = btoa(String.fromCharCode.apply(null, chunk));
-            yield {
-                response: {
+            do {
+                const length = Math.min(fileData.byteLength - position, MAX_PAYLOAD_SIZE);
+                const chunk = new Uint8Array(fileData, position, length);
+                position += length;
+                const base64 = btoa(String.fromCharCode.apply(null, chunk));
+
+                yield {
                     action: 'append',
                     file: file.name,
                     data: base64,
-                },
-            };
+                };
 
-            let percentDone = (fileData.byteLength === 0) ? 100 : Math.round((position / fileData.byteLength) * 100);
-            // UPDATE PROGRESS BAR
-            (async function() {
-                const pythonProgressBar = document.querySelector('div.progress > div.progress-bar');
-                if (pythonProgressBar) {
-                    pythonProgressBar.style.width = percentDone + '%';
-                }
-            })();
+                let percentDone = fileData.byteLength === 0 ? 100 : Math.round((position / fileData.byteLength) * 100);
+                updateProgressBar(percentDone);
 
-        } while (position < fileData.byteLength);
+            } while (position < fileData.byteLength);
+        }
     }
 
-    yield {
-        response: {
-            action: 'complete',
+    async function _uploadFiles(inputId, outputId) {
+        const steps = uploadFilesStep(inputId);
+        const outputElement = document.getElementById(outputId);
+        let result;
+
+        for await (let step of steps) {
+            outputElement.innerHTML = JSON.stringify(step);
+            result = step;
         }
+
+        return result;
+    }
+
+    async function _uploadFilesContinue(outputId) {
+        const outputElement = document.getElementById(outputId);
+        const steps = outputElement.steps;
+        const next = steps.next(outputElement.lastPromiseValue);
+        return Promise.resolve(next.value.promise).then((value) => {
+            outputElement.lastPromiseValue = value;
+            return next.value.response;
+        });
+    }
+
+    scope.google = scope.google || {};
+    scope.google.colab = scope.google.colab || {};
+    scope.google.colab._files = {
+        _uploadFiles,
+        _uploadFilesContinue,
     };
-}
 
-// Expose the functions to the global scope
-scope.google = scope.google || {};
-scope.google.colab = scope.google.colab || {};
-scope.google.colab._files = {
-    _uploadFiles,
-    _uploadFilesContinue,
-};
-
-// Start the file upload process
-_uploadFiles('INPUT_ID_PLACEHOLDER', 'OUTPUT_ID_PLACEHOLDER');
+    _uploadFiles('INPUT_ID_PLACEHOLDER', 'OUTPUT_ID_PLACEHOLDER');
 
 })(self);
